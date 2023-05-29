4e9ceee282635ac46214dcf55251068c1c6ca05b
rl-schun <richardlimsiuchun@gmail.com>
2023-05-29 21:37:25 +0800

    Initial Creation

---
+++ resources/js/components/atom/Header.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+        };
+    },
+};
+</script>
+
+<template>
+    <button class="bg-slate-500" @click="count++">
+        You clicked me {{ count }} times.
+    </button>
+</template>
---
+++ resources/js/components/organism/ExampleComponent.vue
@@ -0,0 +1,20 @@
+<script>
+import Header from "../atom/Header.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+    components: { Header },
+});
+</script>
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+                    <Header />
+                    <div class="card-body">I'm an example component.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
